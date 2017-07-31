b19ebdc3e231e5e9c6bf4e18ba37611b35a5f7b3
errorfourten <yihhang@gmail.com>
2017-08-01 00:18:11 +0800

    Bug fixes

--- telegramCommands.py
+++ telegramCommands.py
@@ -3,7 +3,7 @@ def check_commands(message):
         return "*Help Menu*\n/help - For a list of commands\n/about - About page"
     elif message == "/about":
         return "*About Page*\nThank you for using SingBusBot. Please do check out the code at https://github.com/errorfourten/singbusbot and suggest new ideas you may have."
-    elif message == "/start"
+    elif message == "/start":
         return "Welcome to Sing Bus Bot! Just enter a bus stop code and find the next bus. /help for more information"
     else:
         return False
