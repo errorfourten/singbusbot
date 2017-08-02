0170c90439596b5566a86d4022d3666a08ecbc67
errorfourten <yihhang@gmail.com>
2017-08-02 10:22:14 +0800

    Updated commands

--- telegramCommands.py
+++ telegramCommands.py
@@ -2,8 +2,8 @@ def check_commands(message):
     if message == "/help":
         return "*Help Menu*\n/help - For a list of commands\n/about - About page"
     elif message == "/about":
-        return "*About Page*\nThank you for using SingBusBot. Please do check out the code at https://github.com/errorfourten/singbusbot and suggest new ideas you may have."
+        return "*About Page*\nThank you for using SingBusBot. \nPlease do check out the code at https://github.com/errorfourten/singbusbot and new ideas are always welcome."
     elif message == "/start":
-        return "Welcome to Sing Bus Bot! Just enter a bus stop code and find the next bus. /help for more information"
+        return "Welcome to Singapore Bus Bot! Just enter a bus stop code and find the next bus. For example, try entering 53009 or Bishan Int. /help for more information"
     else:
         return False
