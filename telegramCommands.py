03671c41ea22cf0c61e02dd9d96b66f19d53f50d
errorfourten <yihhang@gmail.com>
2018-02-04 23:41:18 +0800

    Version 2.0

--- telegramCommands.py
+++ telegramCommands.py
@@ -1,15 +1,22 @@
-from singbusbot import send_message_to_owner, get_url
+from singbusbot import send_message_to_owner
+import logging
 
-def check_commands(message):
+def check_commands(bot, update, message):
     message = message.split(" ")
     if message[0] == "/help":
-        return "*Help Menu*\n/help - For a list of commands\n/about - About page\n/feedback - Send feedback straight to the developer by using this format - /feedback [text]"
+        return "<b>Help Menu</b>\n/help - For a list of commands\n/about - About page\n/feedback - Send feedback straight to the developer by using this format - /feedback [text]"
     elif message[0] == "/about":
-        return "*About Page*\nThank you for using SingBusBot. \nPlease do check out the code at https://github.com/errorfourten/singbusbot and new ideas are always welcome."
+        return "<b>About Page</b>\nThank you for using SingBusBot. \nPlease do check out the code at https://github.com/errorfourten/singbusbot and new ideas are always welcome."
     elif message[0] == "/start":
-        return "Welcome to Singapore Bus Bot! Just enter a bus stop code and find the next bus. For example, try entering 53009 or Bishan Int. /help for more information"
+        return "Welcome to Singapore Bus Bot! Just enter a bus stop code and find the next bus. For example, try entering 53009 or Bishan Int.\nDon't know what your bus stop code is? Take a look at the top right of the information boards or check https://www.transitlink.com.sg/eservice/eguide/service_idx.php with your bus number to look up the bus stop code. \n\n /help for more information"
     elif message[0] == "/feedback":
-        send_message_to_owner(" ".join(message[1:]))
-        return "Thank you for your feedback! \"{}\"".format(" ".join(message[1:]))
+        if message == False:
+            return "Please enter feedback"
+        else:
+            if message[1:] == []:
+                return "Please enter some feedback"
+            else:
+                send_message_to_owner(bot, " ".join(message[1:]))
+                return "Thank you for your feedback! \"{}\"".format(" ".join(message[1:]))
     else:
         return False
